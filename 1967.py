6fb3b0e3454a44401e797d49f727ead23cee9ab8
jin <gotgothot@naver.com>
2022-10-09 04:01:54 +0900

    day1

---
+++ 1967.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+n=int(input())
+tree = [[] for _ in range(n+1)]
+for i in range(n-1):
+    a,b,c = map(int,input().split())
+    tree[a].append([b,c])
+    tree[b].append([a,c])
+
+def dfs(node,accumulated_weight):
+    for next_node,weight in tree[node]:
+        if distance[next_node]==-1:
+            distance[next_node]=weight+accumulated_weight
+            dfs(next_node,distance[next_node])
+
+distance = [-1]*(n+1)
+distance[1]=0
+dfs(1,0)
+# print(distance)
+
+start = distance.index(max(distance))
+distance = [-1]*(n+1)
+distance[start]=0
+dfs(start,0)
+
+# print(distance)
+print(max(distance))
