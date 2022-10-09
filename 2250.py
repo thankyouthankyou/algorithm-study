ea8744c9d1e87bebbc594ecddfb62d672087ba96
jin <gotgothot@naver.com>
2022-10-09 13:23:18 +0900

    day2

---
+++ 2250.py
@@ -0,0 +1,37 @@
+n=int(input())
+tree = [[] for _ in range(n+1)]
+row = [[] for _ in range(n+1)]
+is_root = [0]*(n+1)
+num = 1
+
+def inorder(root, level):
+    global num
+    if tree[root][0] != -1:
+        inorder(tree[root][0], level + 1)
+    row[level].append(num)
+    num += 1
+    if tree[root][1] != -1:
+        inorder(tree[root][1], level + 1)
+
+
+for _ in range(n):
+    node,left,right = map(int,input().split())
+    tree[node].append(left)
+    tree[node].append(right)
+    is_root[node]+=1
+    if left!=-1:
+        is_root[left]+=1
+    if right!=-1:
+        is_root[right]+=1
+        
+root = is_root.index(1)
+inorder(root,1)
+
+# print(row)
+level=1
+width=0
+for i in range(len(row)):
+    if row[i] and width<max(row[i])-min(row[i])+1:
+        width=max(row[i])-min(row[i])+1
+        level=i
+print(level,width)
