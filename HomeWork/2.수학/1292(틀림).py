0def735840c22879391c6d44e4af74c7d724832a
jonggking <108375964+jonggking@users.noreply.github.com>
2022-10-16 15:00:54 +0900

    Update 1292(틀림).py

--- HomeWork/2.수학/1292(틀림).py
+++ HomeWork/2.수학/1292(틀림).py
@@ -1,11 +1,7 @@
 L = []
 a, b = map(int, input().split())
-
 for i in range(1000):
-    if i==7:
-        break
-    for j in range(i):
-        L.append(i)
+  for j in range(i): 
+    L.append(i)
     
 print(sum(L[a-1:b]))
-# print(L[999])
