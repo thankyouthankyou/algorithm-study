82ff2db061fbf3fa542646d5b47b597929115a1a
jisil.park <holyjisil@naver.com>
2022-11-01 18:39:03 +0900

    [math] 1-1 2

---
+++ HomeWork/jisil/math_1_1/1978.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, cnt, result;
+vector<int> vec;
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        vec.push_back(tmp);
+    }
+
+    for (int a : vec)
+    {
+        cnt = 0;
+        for (int i = 2; i < 1000; i++)
+        {
+            if ((a % i == 0))
+            {
+                cnt++;
+            }
+            if (cnt > 2)
+                break;
+        }
+        if (cnt == 1)
+        {
+            result++;
+        }
+    }
+    cout << result << endl;
+}
