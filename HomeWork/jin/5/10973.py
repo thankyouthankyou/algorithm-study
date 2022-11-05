f167380e9e4578087d7ad53c76c2cc02865b2e1f
jin <gotgothot@naver.com>
2022-11-06 01:01:43 +0900

    1106

--- HomeWork/jin/5/10973.py
+++ HomeWork/jin/5/10973.py
@@ -4,15 +4,15 @@ n = int(input())
 # li = [i for i in range(1,n+1)]
 li = list(map(int,input().split()))
 
-if li == sorted(li,reverse=True):
+if li == sorted(li):
     print(-1)
 
 for i in range(n-1,0,-1):
-    if li[i-1]<li[i]:
+    if li[i-1]>li[i]:
         for j in range(n-1,0,-1):
-            if li[i-1]<li[j]:
+            if li[i-1]>li[j]:
                 li[i-1],li[j]=li[j],li[i-1]
-                li = li[:i]+sorted(li[i:])
+                li = li[:i]+sorted(li[i:],reverse=True)
                 print(*li)
                 exit()
                 
---
+++ HomeWork/jin/5/1722.py
@@ -0,0 +1,16 @@
+n = int(input())
+li = [i for i in range(1,n+1)]
+inp = list(map(int,input().split()))
+
+if inp[0]==1:
+    for i in range(inp[1]):
+        for i in range(n-1,0,-1):
+            if li[i-1]<li[i]:
+                for j in range(n-1,0,-1):
+                    if li[i-1]<li[j]:
+                        li[i-1],li[j]=li[j],li[i-1]
+                        li = li[:i]+sorted(li[i:],reverse=True)
+                        print(*li)
+                        exit()
+else:
+
