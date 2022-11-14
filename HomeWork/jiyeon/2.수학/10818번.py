49a059828be4b888e6196717474f88567bb33bb3
kdomo <dongho5088@naver.com>
2022-11-14 13:08:16 +0900

    remove : jiyeon Directory

--- HomeWork/jiyeon/2.수학/10818번.py
+++
@@ -1,4 +0,0 @@
-import sys
-n = int(sys.stdin.readline()) # 이게 map보다 더 메모리와 시간상효율적
-nums = list(map(int, sys.stdin.readline().split()))
-print(min(nums), max(nums))
--- HomeWork/jiyeon/2.수학/2693번_N번째큰수.py
+++
@@ -1,5 +0,0 @@
-#정렬
-for _ in range(int(input())):
-    print(sorted(list(map(int, input().split())))[-3])
-
-    #몰라서 찾아봄
