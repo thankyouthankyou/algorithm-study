726f0b3d599ee32c667d4d57f01978b25503dc0f
kdomo <dongho5088@naver.com>
2022-11-15 19:44:23 +0900

    feat : BOJ 10828 스택

---
+++ HomeWork/domo/7.stack_queue_deque_priorityQueue_heap/BOJ_10828_스택.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<String> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            String cmd = str.split(" ")[0];
+            switch (cmd) {
+                case "push":
+                    String value = str.split(" ")[1];
+                    stack.push(value);
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(stack.pop() + "\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size() + "\n");
+                    break;
+                case "empty" :
+                    if(stack.isEmpty()){
+                        sb.append("1\n");
+                    }else{
+                        sb.append("0\n");
+                    }
+                    break;
+                case "top":
+                    if(stack.isEmpty()){
+                        sb.append("-1\n");
+                    }else{
+                        sb.append(stack.peek()+"\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
