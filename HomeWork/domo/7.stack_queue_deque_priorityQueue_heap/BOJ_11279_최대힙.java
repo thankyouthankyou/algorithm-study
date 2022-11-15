1c3f8a26c363c80d9d70b0ed0460637ecc51bf53
kdomo <dongho5088@naver.com>
2022-11-15 20:42:02 +0900

    feat : BOJ 11279 최대힙

---
+++ HomeWork/domo/7.stack_queue_deque_priorityQueue_heap/BOJ_11279_최대힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        PriorityQueue pq = new PriorityQueue(Collections.reverseOrder());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if(pq.isEmpty()){
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.offer(num);
+            }
+        }
+    }
+}
