5ab654e67c4be6bb147f82339adccdd728612499
kdomo <dongho5088@naver.com>
2022-11-15 19:45:13 +0900

    feat : BOJ 9012 괄호

---
+++ HomeWork/domo/7.stack_queue_deque_priorityQueue_heap/BOJ_9012_괄호.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int k = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < k; i++) {
+            sb.append(stack(br.readLine())).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+    static String stack(String s){
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '('){
+                st.push(c);
+            }else if(st.empty()){
+                return "NO";
+            }else{
+                st.pop();
+            }
+        }
+        if(st.empty()){
+            return "YES";
+        }else{
+            return "NO";
+        }
+    }
+}
