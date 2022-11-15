6b9007eb348c39034c654b7133a2dc4bbf41def7
kdomo <dongho5088@naver.com>
2022-11-15 23:05:40 +0900

    update : Pro_LV1_같은 숫자는 싫어

--- HomeWork/domo/7.stack_queue_deque_priorityQueue_heap/PRO_LV1_같은숫자는싫어.java
+++ HomeWork/domo/7.stack_queue_deque_priorityQueue_heap/PRO_LV1_같은숫자는싫어.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class PRO_LV1_같은숫자는싫어 {
-    public static void main(String[] args) {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.add(arr[0]);
 
+        for(int i=1; i<arr.length; i++){
+            if(!stack.peek().equals(arr[i])){
+                stack.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+
+        for(int i=stack.size()-1; i>=0; i--){
+            answer[i] = stack.pop();
+        }
+
+        return answer;
     }
+
 }
