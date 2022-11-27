ffc3024af459a4c9a9f26a8c512fc80d0e54fcfd
kdomo <dongho5088@naver.com>
2022-11-27 21:12:51 +0900

    feat : BOJ_1654_랜선자르기

---
+++ HomeWork/domo/8.sort_binarySearch_twoPointer/BOJ_1654_랜선자르기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1654_랜선자르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int K = Integer.parseInt(st.nextToken());
+        long N = Long.parseLong(st.nextToken());
+        long[] arr = new long[K];
+
+        long max = 0;
+        for (int i = 0; i < K; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        long left = 1;
+        long right = max;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+            for (int i = 0; i < K; i++) {
+                sum += arr[i]/mid;
+            }
+
+            if (sum >= N) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        System.out.println(right);
+    }
+}
