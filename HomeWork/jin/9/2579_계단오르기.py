7a3cdc4a5b13dffd40d8c4a78f9db20b54791455
jin <gotgothot@naver.com>
2022-12-04 23:50:32 +0900

    1205

--- HomeWork/jin/9/2579_계단오르기.py
+++ HomeWork/jin/9/2579_계단오르기.py
@@ -1,18 +1,12 @@
-import sys
-input=sys.stdin.readline
+n = int(input())
+li = list(int(input()) for _ in range(n))
 
-n=int(input())
-dp = [0 for i in range(301)]
-# array=[0]+[int(input()) for _ in range(n)]
-array=[0 for i in range(301)]
+dp=[li[0]]
+if n>1:
+    dp.append(li[0]+li[1])
+if n>2:
+    dp.append(max(li[0]+li[2],li[1]+li[2]))
 
-for i in range(n):
-    array[i+1]=int(input())
-
-dp[1]=array[1]
-dp[2]=array[1]+array[2]
-
-for i in range (3,n+1):
-    dp[i]=max(dp[i-3]+array[i-1]+array[i],dp[i-2]+array[i])
-
-print(dp[n])
+for i in range (3,n):
+    dp.append(max(dp[i-3]+li[i-1]+li[i],dp[i-2]+li[i]))
+print(dp[n-1])
