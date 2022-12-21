e3bed1b07af489194f094df815c58e23e9567db3
kdomo <dongho5088@naver.com>
2022-12-21 18:32:23 +0900

    feat : BOJ_2748_피보나치수2

---
+++ HomeWork/domo/9.DivideAndConquer_DP/BOJ_2748_피보나치수2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2748_피보나치수2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[N]);
+    }
+}
